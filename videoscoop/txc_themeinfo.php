9e06415be2ecf9b2d2287d529512b6667eaa7969
mambax7 <mambax7@gmail.com>
2015-05-04 05:50:20 -0400

    Original files

---
+++ videoscoop/txc_themeinfo.php
@@ -0,0 +1,6 @@
+<?php
+	$txc_themeinfo["name"] = "videoscoop";
+	$txc_themeinfo["author"] = "montisarts";
+	$txc_themeinfo["author_url"] = "http://www.montisarts.com/";
+	$txc_themeinfo["detail_url"] = "http://xoops.org.cn/modules/wfdownloads/visit.php?cid=6&lid=160";
+?>
